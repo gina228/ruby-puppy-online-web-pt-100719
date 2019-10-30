6a5fc1df0ccdfb1aec5b333ee9ea1ac0962197ee
Gina Graci <ginagraci92@gmail.com>
2019-10-30 23:42:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize
+  end
+  
+  def all
+  end 
+
+  def print_all
+    @@all
+  end 
+  
+  def clear_all
+  end 
+    
+    
+end
